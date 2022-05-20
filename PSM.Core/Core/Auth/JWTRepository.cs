4286948b4a42b32313ba62f618fab0c732ef0bd0
Matthew <matthew@tfaluc.com>
2022-05-20 19:56:11 -0400

    code cleanup

--- PSM.Core/Core/Auth/JWTRepository.cs
+++ PSM.Core/Core/Auth/JWTRepository.cs
@@ -3,49 +3,45 @@ using PSM.Core.Core.Database;
 using PSM.Core.Models;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using PSM.Core.Models.Auth;
 
 namespace PSM.Core.Core.Auth {
-    public interface IJWTRepository {
-        TokenResponseModel Authenticate(User user);
-		User? UserFromContext(HttpContext context);
+  public interface IJWTRepository {
+    ClientTokenModel Authenticate(User           user);
+    User?            UserFromContext(HttpContext context);
+  }
+
+  public class JWTRepository : IJWTRepository {
+    private readonly PSMContext _dbc;
+
+    public JWTRepository(PSMContext dbc) {
+      _dbc = dbc;
     }
 
-    public class JWTRepository : IJWTRepository {
-        private readonly PSMContext dbc;
-        public JWTRepository(PSMContext _dbc) {
-            dbc = _dbc;
-        }
+    public ClientTokenModel Authenticate(User user) {
+      var tokenHandler   = new JwtSecurityTokenHandler();
+      var expirationTime = DateTime.UtcNow.AddMinutes(15); // 15 minute lifetime
+      var tokenDescriptor = new SecurityTokenDescriptor {
+                                                          Subject = new ClaimsIdentity(new[] {
+                                                                                               new Claim("id", user.Id.ToString())
+                                                                                             }),
+                                                          Expires            = expirationTime,
+                                                          SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Constants.GetJWTBytes()), SecurityAlgorithms.HmacSha256Signature)
+                                                        };
 
-		public TokenResponseModel Authenticate(User user) {
-			JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-			DateTime expiration_time = DateTime.UtcNow.AddMinutes(15); // 15 minute lifetime
-			SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor {
-				Subject = new ClaimsIdentity(new Claim[] {
-					new Claim("id", user.Id.ToString())
-				}),
-				Expires = expiration_time, 
-				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Constants.GetJWTBytes()), SecurityAlgorithms.HmacSha256Signature)
-			};
-
-			SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
-			return new TokenResponseModel { Token = tokenHandler.WriteToken(token), ExpiresAt = expiration_time };
-		}
-
-        public User? UserFromContext(HttpContext context) {
-            ClaimsIdentity? identity = context.User.Identity as ClaimsIdentity;
-            
-            if (identity != null) {
-                int userid = int.Parse(identity.FindFirst("id").Value);
-                if (dbc.Users.Where(x => x.Id == userid).Any()) {
-                    User user = dbc.Users.Where(x => x.Id == userid).First();
-                    // By only returning enabled users, we can handle disabling users while tokens are still alive
-                    if(user.Enabled) {
-                        return user;
-                    }
-                }
-            }
-
-            return null;
-        }
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return new ClientTokenModel { Token = tokenHandler.WriteToken(token), ExpiresAt = expirationTime };
     }
+
+    public User? UserFromContext(HttpContext context) {
+      if(context.User.Identity is not ClaimsIdentity identity)
+        return null;
+      if(!int.TryParse(identity.FindFirst("id").Value, out var userid))
+        return null;
+      if(_dbc.Users.FirstOrDefault(x => x.Id == userid) is not { } user)
+        return null;
+      // By only returning enabled users, we can handle disabling users while tokens are still alive
+      return user.Enabled ? user : null;
+    }
+  }
 }
