02bda0335996c824b0bf1df91dd039fabe7f9a16
Matthew <matthew@tfaluc.com>
2022-05-21 16:55:01 -0400

    refactor PSMContext

--- PSM.Core/Core/Database/PSMContext.cs
+++ PSM.Core/Core/Database/PSMContext.cs
@@ -1,49 +1,59 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using PSM.Core.Models;
 
 namespace PSM.Core.Core.Database {
-    public class PSMContext : DbContext {
-        public PSMContext(DbContextOptions<PSMContext> options) : base(options) { }
-        public DbSet<User> Users { get; set; }
+  [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+  public class PSMContext : DbContext {
+    public PSMContext(DbContextOptions<PSMContext> options) : base(options) { }
+    public DbSet<User>          Users          { get; set; } = null!;
+    public DbSet<PermissionSet> PermissionSets { get; set; } = null!;
 
-        /// <summary>
-        /// Adds the default system user and an admin user
-        /// </summary>
-        public bool SeedPSMUsers(ILogger logger) {
-            // Add system user first. Used by automated jobs & for the admin user
-            bool workdone = false;
+    /// <summary>
+    /// Adds the default system user and an admin user
+    /// </summary>
+    public bool SeedPSMUsers(ILogger logger) {
+      var seedingOccured = false;
+      // Check for the system user and generate if not found
+      if(Users.FirstOrDefault(user => user.Username.Equals(Constants.System.SystemUsername, StringComparison.Ordinal)) is not { } systemUser) {
+        systemUser = new User {
+                                Enabled      = false,
+                                Username     = Constants.System.SystemUsername,
+                                PasswordHash = "_" // Impossible to get via hashes
+                              };
+        systemUser.PermissionSet = new PermissionSet { Owner = systemUser.Id, Permissions = Constants.AllPermissions };
+        Users.Add(systemUser);
+        systemUser.PermissionSet.MapOwner(this);
+        logger.LogInformation("Created system user");
+        seedingOccured = true;
+        SaveChanges();
+      }
 
-            User system_user;
-            if (!Users.Where(x => x.Username.Equals(Constants.SYSTEM_USER_NAME)).Any()) {
-                system_user = new User();
-                system_user.Enabled = false;
-                system_user.Username = Constants.SYSTEM_USER_NAME;
-                system_user.PasswordHash = "_"; // Impossible to get via hashes
-                Users.Add(system_user);
-                logger.LogInformation("Seeded system user.");
-                workdone = true;
-                SaveChanges();
-            } else {
-                system_user = Users.Where(x => x.Username.Equals(Constants.SYSTEM_USER_NAME)).First();
-            }
+      if(systemUser.Enabled) {
+        // This should never be enabled
+        logger.LogCritical("System User is enabled");
+        Environment.Exit(Constants.ExitCodes.SystemUserEnabled);
+        return false;
+      }
 
-            // Add the default admin user
-            if (!Users.Where(x => x.Username.Equals(Constants.ADMIN_USER_NAME)).Any()) {
-                User admin_user = new User();
-                admin_user.Enabled = true;
-                PasswordHasher<User> hasher = new PasswordHasher<User>();
-                admin_user.Username = Constants.ADMIN_USER_NAME;
-                admin_user.PasswordHash = hasher.HashPassword(admin_user, Constants.DEFAULT_ADMIN_PASS);
-                admin_user.CreatedBy = system_user;
-                logger.LogInformation("Seeded default admin user.");
-                Users.Add(admin_user);
-                workdone = true;
-                SaveChanges();
-            }
+      if(Users.FirstOrDefault(user => user.Username.Equals(Constants.System.SystemAdminUsername, StringComparison.Ordinal)) is not { } adminUser) {
+        adminUser = new User {
+                               Enabled   = true,
+                               Username  = Constants.System.SystemAdminUsername,
+                               CreatedBy = systemUser
+                             };
+        adminUser.PasswordHash  = new PasswordHasher<User>().HashPassword(adminUser, Constants.System.SystemAdminPassword);
+        adminUser.PermissionSet = new PermissionSet { Owner = adminUser.Id, Permissions = Constants.AllPermissions };
+        Users.Add(adminUser);
+        adminUser.PermissionSet.MapOwner(this);
+        logger.LogInformation("Created default admin user");
+        seedingOccured = true;
+        SaveChanges();
+      }
 
-            return workdone;
-        }
+      SaveChanges();
+      return seedingOccured;
     }
+  }
 }
