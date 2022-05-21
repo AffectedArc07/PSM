976c0967329601339e560ac6d138442644c048d0
Matthew <matthew@tfaluc.com>
2022-05-21 16:55:55 -0400

    refactor and move User.cs

--- PSM.Core/Core/Database/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSM.Core.Models {
-    public class User {
-        public int Id { get; set;  } 
-        public bool Enabled { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public User? CreatedBy { get; set; }
-    }
-}
