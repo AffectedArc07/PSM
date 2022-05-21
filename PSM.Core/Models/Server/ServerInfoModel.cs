32a8123c3e2a9b66dc012fade324edd85df7f345
Matthew <matthew@tfaluc.com>
2022-05-20 20:06:59 -0400

    move and refactor ServerInfoModel

---
+++ PSM.Core/Models/Server/ServerInfoModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PSM.Core.Models.Server; 
+
+public class ServerInfoModel : ModelBase {
+  public string? ServerVersion, CurrentUsername;
+
+  protected override bool ValidateModel() => true; // We don't do any validation here.
+}
--- PSM.Core/Models/ServerInfoModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace PSM.Core.Models {
-    public class ServerInfoModel {
-        public string? current_username { get; set; }
-        public string? server_version { get; set; }
-    }
-}
