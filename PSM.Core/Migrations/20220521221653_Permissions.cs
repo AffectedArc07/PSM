f654c86bcc40757e999e607ef36fa70c2b6b9e02
ZephyrTFA <matthew@tfaluc.com>
2022-05-22 15:19:07 -0400

    migrations dont auto add to git, agony

---
+++ PSM.Core/Migrations/20220521221653_Permissions.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSM.Core.Migrations
+{
+    public partial class Permissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Users_CreatedById",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_CreatedById",
+                table: "users",
+                newName: "IX_users_CreatedById");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "permission_sets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PermissionString = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permission_sets", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_users_CreatedById",
+                table: "users",
+                column: "CreatedById",
+                principalTable: "users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_users_CreatedById",
+                table: "users");
+
+            migrationBuilder.DropTable(
+                name: "permission_sets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "Users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_users_CreatedById",
+                table: "Users",
+                newName: "IX_Users_CreatedById");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Users_CreatedById",
+                table: "Users",
+                column: "CreatedById",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
