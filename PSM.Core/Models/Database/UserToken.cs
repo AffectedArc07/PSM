42bc927244d18b4cb2b84aa5e16105de4e997038
ZephyrTFA <matthew@tfaluc.com>
2022-05-22 15:19:34 -0400

    token table model

---
+++ PSM.Core/Models/Database/UserToken.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using PSM.Core.Core.Auth;
+
+namespace PSM.Core.Models.Database;
+
+[Table("user_tokens")]
+public class UserToken {
+  public int            Id                { get; set; }
+  public DateTimeOffset ExpiresAt         { get; set; }
+  public string         TokenValue        { get; set; }
+  public string         OriginatorAddress { get; set; }
+  public bool           OriginatorRoaming { get; set; } = false;
+}
