165e627fdf012e5ed14851831e629a9e5b33ff44
ZephyrTFA <matthew@tfaluc.com>
2022-05-23 07:32:49 -0400

    refactor api_manager

--- PSM.Core/ClientApp/src/js/core/api_manager.js
+++ PSM.Core/ClientApp/src/js/core/api_manager.js
@@ -1,24 +1,29 @@
 import axios from 'axios';
-import { notification } from 'antd';
+import {notification} from 'antd';
 
 class ApiManager {
   constructor() {
-    console.log("[APIM] Starting API manager");
+    console.log("[API-M] Starting API manager");
     this.bearer = null;
-    this.expiration_date = null;
+    this.bearerId = null;
+    this.bearerExp = null;
     // We handle our own errors here
-    this.axs = axios.create({ validateStatus: function (status) { return true; } });
-    console.log("[APIM] Ready");
+    this.axs = axios.create({
+      validateStatus: function () {
+        return true;
+      }
+    });
+    console.log("[API-M] Ready");
   }
 
   valid_token() {
     // If we are missing both of these, we fail anyway
-    if (!this.bearer || !this.expiration_date) {
+    if (!this.bearer || !this.bearerExp) {
       return false;
     }
 
-    // If our token expired, we aint valid
-    if (new Date().getTime() > this.expiration_date) {
+    // If our token expired, we ain't valid
+    if (new Date().getTime() > this.bearerExp) {
       return false;
     }
 
@@ -46,7 +51,11 @@ class ApiManager {
 
     // If we are here we have our data
     this.bearer = response.data["token"];
-    this.expiration_date = Date.parse(response.data["expiresAt"]);
+    this.bearerId = response.data["userId"]
+    this.bearerExp = Date.parse(response.data["expiresAt"]);
+    this.axs.defaults.headers.common = {
+      Authorization: `Bearer${this.bearerId !== 0 ? `(${this.bearerId})` : ``} ${this.bearer}`
+    }
     this.reloadState(1);
   }
 
@@ -58,7 +67,12 @@ class ApiManager {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-
+  async debug_verify_token() {
+    alert("test")
+    let res = await this.axs.post("/api/auth/debug_verify")
+    if (res.status !== 200)
+      alert("failed to validate")
+  }
 }
 
 export const API = new ApiManager();
