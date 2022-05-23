cf127a69ca0932dddb32c3bab56eb130a48a4178
ZephyrTFA <matthew@tfaluc.com>
2022-05-23 07:36:20 -0400

    helper method for validating tokens

--- PSM.Core/Models/Database/UserToken.cs
+++ PSM.Core/Models/Database/UserToken.cs
@@ -1,5 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Net.Http.Headers;
+using PSM.Core.Core;
 using PSM.Core.Core.Auth;
+using PSM.Core.Core.Database;
 
 namespace PSM.Core.Models.Database;
 
@@ -10,4 +13,26 @@ public class UserToken {
   public string         TokenValue        { get; set; }
   public string         OriginatorAddress { get; set; }
   public bool           OriginatorRoaming { get; set; } = false;
+
+  public static bool AttemptValidateToken(HttpContext context, IJWTRepository jwtRepository, PSMContext psmContext) {
+    UserToken? userToken = null;
+    try {
+      var user = jwtRepository.UserFromContext(context);
+      userToken = psmContext.UserTokens.FirstOrDefault(userTok => userTok.Id == user.Id);
+    } catch(Exception e) {
+      Constants.AppLog.LogCritical("Exception validating Token: {Exception}", e);
+    }
+
+    if(userToken is null)
+      return false;
+
+    if(!userToken.OriginatorRoaming && userToken.OriginatorAddress != context.Connection.RemoteIpAddress.ToString())
+      return false;
+
+    var token = context.Request.Headers[HeaderNames.Authorization];
+    if(!userToken.TokenValue.Equals(token))
+      return false;
+
+    return true;
+  }
 }
