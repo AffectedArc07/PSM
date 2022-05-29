8badfdbd688b66631b19da8b5af416dad5fdb6a8
ZephyrTFA <matthew@tfaluc.com>
2022-05-28 23:06:17 -0400

    create and implement backendState management

--- PSM.Core/ClientApp/src/js/app.js
+++ PSM.Core/ClientApp/src/js/app.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import { Login } from './pages/Login';
-import { MainApp } from './pages/MainApp';
-import { API } from './core/api_manager';
+import React, {useState} from 'react';
+import {Login} from './pages/Login';
+import {API} from './core/api_manager';
+import Backend from "./components/backend";
+
+Backend.Global.setBackend("navTab", 0);
+Backend.Global.setBackend("navTabs", [
+  "Login", "Users"
+]);
 
 export const App = () => {
-  const [forceRefresh, setForceRefresh] = useState(0);
-
+  const [, setForceRefresh] = useState(0);
+  const [navTab] = Backend.Global.useBackend(this, "navTab", 0);
   API.setReloadState(setForceRefresh);
 
-  if (!API.valid_token()) {
-    return (
-      <Login />
-    );
+  switch (navTab) {
+    case 0:
+    default:
+      return <Login/>
+    case 1:
+      return <Users/>
   }
-
-  return <MainApp />;
 };
