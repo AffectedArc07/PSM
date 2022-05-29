e9629dfe7b17a6e66c3c64aeccbf179786cb5c46
ZephyrTFA <matthew@tfaluc.com>
2022-05-29 14:22:39 -0400

    merge ui dev branch and refactor to tsx

--- PSM.Core/ClientApp/src/js/pages/Login.tsx
+++ PSM.Core/ClientApp/src/js/pages/Login.tsx
@@ -1,62 +1,66 @@
-import React, { useState } from 'react';
-import { LoginOutlined } from '@ant-design/icons';
-import { Button, Card, Col, Form, Input, Row, Space } from 'antd';
-import { API } from '../core/api_manager';
+import React, {useState} from 'react';
+import {LoginOutlined} from '@ant-design/icons';
+import {Button, Card, Col, Form, Input, Row, Space} from 'antd';
+import {API, LoginForm} from '../core/api_manager';
 
 export const Login = () => {
   const [locked, setLocked] = useState(false);
 
-  const attemptLogin = async values => {
+  const attemptLogin = async (values: LoginForm) => {
     setLocked(true);
     await API.attempt_login(values);
     setLocked(false);
   };
 
-  const attemptDefaultLogin = async values => {
+  const attemptDefaultLogin = async () => {
     setLocked(true);
-    await API.attempt_login({"username": "ADMIN", "password": "ChangeMeYouMuppet"});
+    await API.attempt_login({
+      username: "ADMIN",
+      password: "ChangeMeYouMuppet",
+    });
     setLocked(false);
   };
 
   return (
-    <Row align="middle" justify="center" style={{ height: '100%' }}>
+    <Row align="middle" justify="center" style={{height: '100%'}}>
       <Col span={8}>
         <Space
           direction="vertical"
           size="large"
-          style={{ width: '100%' }}>
+          style={{width: '100%'}}>
           <Card title="PSM Login">
             <Form
               name="login"
               layout="vertical"
               autoComplete="off"
-              initialValues={{ remember: true }}
+              initialValues={{remember: true}}
               onFinish={attemptLogin}>
               <Form.Item
                 label="Username"
                 name="username"
-                rules={[{ required: true }]}>
-                <Input />
+                rules={[{required: true}]}>
+                <Input/>
               </Form.Item>
               <Form.Item
                 label="Password"
                 name="password"
-                rules={[{ required: true }]}>
-                <Input.Password />
+                rules={[{required: true}]}>
+                <Input.Password/>
               </Form.Item>
               <Button
                 type="primary"
                 htmlType="submit"
                 loading={locked}
-                icon={<LoginOutlined />}>
+                icon={<LoginOutlined/>}>
                 Sign in
               </Button>
               <Button
+                // @ts-ignore
                 type="danger"
-                style={{ marginLeft: "4px" }}
+                style={{marginLeft: "4px"}}
                 onClick={() => attemptDefaultLogin()}
                 loading={locked}
-                icon={<LoginOutlined />}>
+                icon={<LoginOutlined/>}>
                 Default Credentials
               </Button>
             </Form>
