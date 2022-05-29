5fb3c50134aa1cbe0288cd29040f733d5ff1e866
ZephyrTFA <matthew@tfaluc.com>
2022-05-28 22:58:27 -0400

    tsx support for webpack

--- PSM.Core/ClientApp/webpack.config.js
+++ PSM.Core/ClientApp/webpack.config.js
@@ -11,6 +11,10 @@ module.exports = smp.wrap({
     filename: "[name].[contenthash].js",
   },
 
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src/html/index.html"),
@@ -34,6 +38,10 @@ module.exports = smp.wrap({
           presets: ['@babel/preset-react'],
         },
       },
+    }, {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
     }, {
       test: /\.css$/,
       use: ['style-loader', 'css-loader'],
