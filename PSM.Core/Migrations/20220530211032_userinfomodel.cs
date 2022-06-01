20d40c55f359d0c6c734bc2fc7262eda2e4dc286
ZephyrTFA <matthew@tfaluc.com>
2022-05-31 22:06:53 -0400

    almost forgot these

---
+++ PSM.Core/Migrations/20220530211032_userinfomodel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSM.Core.Migrations
+{
+    public partial class userinfomodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
