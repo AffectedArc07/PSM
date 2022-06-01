a18abd25155b3b2a38a017a4e8e546af910ac709
ZephyrTFA <matthew@tfaluc.com>
2022-05-31 22:02:45 -0400

    until I get around to working on instances, this is my debug panel

--- PSM.Core/ClientApp/src/js/pages/InstanceView.tsx
+++ PSM.Core/ClientApp/src/js/pages/InstanceView.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Main} from "./Main";
 
 export const InstanceView = () => {
   return (
     <div style={{ "margin": "16px" }}>
       <h1>Instances</h1>
+      <button onClick={()=>Main.app_error({error: 'test error', recoverable: false})}>Test Non-Recoverable Error</button>
+      <button onClick={()=>Main.app_error({error: 'test error', recoverable: true})}>Test Recoverable Error</button>
     </div>
   )
 }
