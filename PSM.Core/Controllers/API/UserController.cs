974da7ea855e99c81b0a226f9a97344eff4a02cd
ZephyrTFA <matthew@tfaluc.com>
2022-05-31 21:58:25 -0400

    user controller

---
+++ PSM.Core/Controllers/API/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PSM.Core.Core;
+using PSM.Core.Core.Auth;
+using PSM.Core.Core.Database;
+using PSM.Core.Models.API;
+
+namespace PSM.Core.Controllers.API;
+
+[Route("api/users")]
+public class UserController : Controller {
+  private readonly PSMContext     _dbc;
+  private readonly IJWTRepository _jwt;
+
+  public UserController(PSMContext dbc, IJWTRepository jwt) {
+    _dbc = dbc;
+    _jwt = jwt;
+  }
+
+  [HttpGet("list")]
+  [ProducesResponseType(typeof(UserInformationModel[]), 200)]
+  public IActionResult ListUsers() {
+    var user = _jwt.UserFromContext(HttpContext);
+    if(user is null || !user.PermissionSet.Contains(PSMPermission.UserList)) {
+      return Forbid();
+    }
+
+    var users = _dbc.Users.Select(dbUser => new UserInformationModel {
+                                                                       Username = dbUser.Username,
+                                                                       Enabled  = dbUser.Enabled,
+                                                                       UserID   = dbUser.Id
+                                                                     }).ToArray();
+    return Ok(users);
+  }
+}
---
+++ PSM.Core/Models/API/UserInformationModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PSM.Core.Models.API;
+
+public class UserInformationModel : ModelBase {
+  public string? Username { get; set; }
+  public int?    UserID   { get; set; } = -1;
+  public bool?   Enabled  { get; set; }
+}
