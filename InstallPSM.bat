67dbd08fa9db52fc735603015dd4f73738f05cdc
ZephyrTFA <matthew@tfaluc.com>
2022-06-20 23:02:10 -0400

    I am experiencing mild agony

--- InstallPSM.bat
+++ InstallPSM.bat
@@ -1,17 +1,7 @@
 @echo off
 
-if exist .git\ (
-  rem nothing here
-) else (
-  git init
-  git remote add origin git@github.com:AffectedArc07/PSM.git
-)
-
-git fetch --all
-git reset --hard
-git switch master
+git fetch origin
 git pull
-
 git submodule init
 git submodule update
 
